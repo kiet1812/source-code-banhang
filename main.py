b508c7e24a80f2e8d4b37a498017f872f3a849ec
kiet1812 <phamkiet1812@gmail.com>
2025-03-27 15:24:43 +0700

    Update

--- main.py
+++ main.py
@@ -9,13 +9,36 @@ class db:
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT,
                 address TEXT,
-                phone_numbers TEXT
+                phone_numbers TEXT,
+                rank TEXT
             )
         """)
-    def insert_users(self, name, address, phone_numbers):
+    def insert_users(self, name, address, phone_numbers, rank = 'member'):
         self.cursor.execute("""
-            INSERT INTO Users(name, address, phone_numbers)
-            VALUE('{}', '{}', '{}')
-        """.format(name, address, phone_numbers))
+            INSERT INTO Users(name, address, phone_numbers, rank)
+            VALUES('{}', '{}', '{}', '{}');
+        """.format(name, address, phone_numbers, rank))
+        self.database.commit()
+    def print_table(self):
+        self.cursor.execute("SELECT * FROM Users")
+        rows = self.cursor.fetchall()
+        print('ĐỊNH DẠNG: ID | NAME | ADDRESS | PHONE NUMBERS | RANK')
+        for row in rows: 
+            print(f'{row[0]} | {row[1]} | {row[2]} | {row[3]} | {row[4]}')
+    def delete_users(self, id):
+        self.cursor.execute("""
+            DELETE FROM Users
+            WHERE id = {}
+        """.format(id))
+        self.database.commit()
+    def count_users(self):
+        self.cursor.execute("SELECT COUNT(*) FROM Users")
+        count = self.cursor.fetchone()
+        print(count[0])
+
 
 data = db()
+# data.insert_users('Pham Tuan Kiet', '755 - Dong Tai - Van Thang - Nong Cong - Thanh Hoa', '0396138606', 'Chu quan')
+# data.delete_users(1)
+# data.print_table()
+data.count_users()
